27a8e637e53bae9c1756ee9d1fd1366df5650354
leiixia <leah.hiff@gmail.com>
2023-09-14 12:43:37 +0200

    update getNearAttraction method

---
+++ src/main/java/com/openclassrooms/tourguide/DTO/AttractionDTO.java
@@ -0,0 +1,69 @@
+package com.openclassrooms.tourguide.DTO;
+
+public class AttractionDTO {
+
+    public String attractionName;
+    public double attractionLatitude;
+    public double attractionLongitude;
+    public double userLatitude;
+    public double userLongitude;
+    public double milesDistance;
+    public int rewardPoints;
+
+    public double getAttractionLatitude() {
+        return attractionLatitude;
+    }
+
+    public void setAttractionLatitude(double attractionLatitude) {
+        this.attractionLatitude = attractionLatitude;
+    }
+
+    public double getAttractionLongitude() {
+        return attractionLongitude;
+    }
+
+    public void setAttractionLongitude(double attractionLongitude) {
+        this.attractionLongitude = attractionLongitude;
+    }
+
+    public double getUserLatitude() {
+        return userLatitude;
+    }
+
+    public void setUserLatitude(double userLatitude) {
+        this.userLatitude = userLatitude;
+    }
+
+    public double getUserLongitude() {
+        return userLongitude;
+    }
+
+    public void setUserLongitude(double userLongitude) {
+        this.userLongitude = userLongitude;
+    }
+
+    public double getMilesDistance() {
+        return milesDistance;
+    }
+
+    public void setMilesDistance(double milesDistance) {
+        this.milesDistance = milesDistance;
+    }
+
+    public int getRewardPoints() {
+        return rewardPoints;
+    }
+
+    public void setRewardPoints(int rewardPoints) {
+        this.rewardPoints = rewardPoints;
+    }
+
+
+    public String getAttractionName() {
+        return attractionName;
+    }
+
+    public void setAttractionName(String attractionName) {
+        this.attractionName = attractionName;
+    }
+}
---
+++ src/main/java/com/openclassrooms/tourguide/TourGuideController.java
@@ -0,0 +1,73 @@
+package com.openclassrooms.tourguide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import gpsUtil.location.Attraction;
+import gpsUtil.location.VisitedLocation;
+
+import com.openclassrooms.tourguide.service.TourGuideService;
+import com.openclassrooms.tourguide.user.User;
+import com.openclassrooms.tourguide.user.UserReward;
+
+import tripPricer.Provider;
+
+@RestController
+public class TourGuideController {
+
+	@Autowired
+	TourGuideService tourGuideService;
+	
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from TourGuide!";
+    }
+    
+    @RequestMapping("/getLocation") 
+    public VisitedLocation getLocation(@RequestParam String userName) {
+    	return tourGuideService.getUserLocation(getUser(userName));
+    }
+    
+    //  TODO: Change this method to no longer return a List of Attractions.
+ 	//  Instead: Get the closest five tourist attractions to the user - no matter how far away they are.
+ 	//  Return a new JSON object that contains:
+    	// Name of Tourist attraction, 
+        // Tourist attractions lat/long, 
+        // The user's location lat/long, 
+        // The distance in miles between the user's location and each of the attractions.
+        // The reward points for visiting each Attraction.
+        //    Note: Attraction reward points can be gathered from RewardsCentral
+
+    //modif ici
+    @RequestMapping(value = "/getNearbyAttractions", method = RequestMethod.GET)
+    @ResponseBody
+    public String getNearbyAttractions(@RequestParam String userName) throws JsonProcessingException {
+    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
+        List<Attraction> attractions = tourGuideService.getNearByAttractions(visitedLocation);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String attractionJson = objectMapper.writeValueAsString(attractions);
+        return attractionJson;
+    }
+    
+    @RequestMapping("/getRewards") 
+    public List<UserReward> getRewards(@RequestParam String userName) {
+    	return tourGuideService.getUserRewards(getUser(userName));
+    }
+       
+    @RequestMapping("/getTripDeals")
+    public List<Provider> getTripDeals(@RequestParam String userName) {
+    	return tourGuideService.getTripDeals(getUser(userName));
+    }
+    
+    private User getUser(String userName) {
+    	return tourGuideService.getUser(userName);
+    }
+   
+
+}
